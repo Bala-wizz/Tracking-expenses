615535f07f54700411167701009b93594b2fd5f3
Rupa Balaraman <rupa.balaraman@gmail.com>
2021-12-21 11:08:15 -0500

    adding children props to Card component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import Expenses from "./components/Expenses";
 
 function App() {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Toilet Paper",
+      amount: 94.67,
+      date: new Date(2020, 7, 14),
+    },
+    {
+      id: "e2",
+      title: "Car Insurance",
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+    { id: "e3", title: "New TV", amount: 794.76, date: new Date(2021, 2, 28) },
+    {
+      ID: "e4",
+      title: "New Desk(Wooden)",
+      amount: 550,
+      date: new Date(2021, 5, 12),
+    },
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2> Lets get started</h2>
+      <Expenses expenses={expenses} />
     </div>
   );
 }
